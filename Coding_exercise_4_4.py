f805b6fb120df653937fdb19c5176fdd1f440ddd
iamrja <rj123alv@gmail.com>
2022-05-26 23:50:44 +0800

    Coding Exercise for 4.4

---
+++ Coding_exercise_4_4.py
@@ -0,0 +1,8 @@
+from functionfile import addNumbers as mat1
+from functionfile import multiplyNumbers as mat2
+
+sum = mat1(1,9)
+print(sum)
+
+product = mat2(2,3)
+print(product)
---
+++ functionfile.py
@@ -0,0 +1,17 @@
+from tkinter import Y
+
+
+def addNumbers(x,y):
+    return x + y
+
+def minusNumbers(x,y):
+    return x - y
+
+def multiplyNumbers(x,y):
+    return x * y
+
+def divideNumbers(x,y):
+    return x / y
+
+def getRemainder(x,y):
+    return x % y
